e0f661a381a863f425dc1ee9913465e997bb2587
hakankaraahmet <karaahmethkn@gmail.com>
2022-09-28 21:55:07 +0300

    routing header is completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccessoriesCardsComponent } from './accessories-cards/accessories-cards.component';
+import { IphoneCardsComponent } from './iphone-cards/iphone-cards.component';
+import { OffersComponent } from './offers/offers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/offers', pathMatch: 'full' },
+  { path: 'offers', component: OffersComponent },
+  { path: 'iphone-cards', component: IphoneCardsComponent },
+  { path: 'accessories-cards', component: AccessoriesCardsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
