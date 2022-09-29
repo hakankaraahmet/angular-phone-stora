7b4558366663a76fc1f544ecbe0fd4024479d5b0
hakankaraahmet <karaahmethkn@gmail.com>
2022-09-29 12:00:56 +0300

    update,add and delete buttons are added

--- src/app/iphone-cards/iphone-cards.component.ts
+++ src/app/iphone-cards/iphone-cards.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Iphone } from './iphone.model';
-import * as fromIphone from './store/iphone-cards.reducer';
+import * as fromApp from '../store/app.reducer';
 
 @Component({
   selector: 'app-iphone-cards',
@@ -10,14 +10,12 @@ import * as fromIphone from './store/iphone-cards.reducer';
   styleUrls: ['./iphone-cards.component.css'],
 })
 export class IphoneCardsComponent implements OnInit {
-  iphoneList: any;
+  iphoneList: Observable<{ ingredients: Iphone[] }> | any;
 
-  constructor(private store: Store<fromIphone.State>) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   ngOnInit(): void {
-    // this.iphoneList = this.store.select('iphoneList');
-   this.store.subscribe(s => this.iphoneList = s)
-   this.iphoneList = this.iphoneList.iphone.iphoneList
-   console.log(this.iphoneList)
+    this.iphoneList = this.store.select('iphone');
+    this.store.subscribe((s) => (this.iphoneList = s.iphone.iphoneList));
   }
 }
