a33053a0a657b8868d796386a7c3fd11bf9bcba1
hakankaraahmet <karaahmethkn@gmail.com>
2022-10-04 05:20:45 -0700

    logged out functionality is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,43 @@
-import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+import { Router } from '@angular/router';
 import { FirebaseService } from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'myApp';
   isSignedIn = false;
-  constructor(public firebaseService : FirebaseService){}
+  constructor(
+    public router: Router,
+    public firebaseService: FirebaseService,
+    public afAuth: AngularFireAuth
+  ) {}
 
   ngOnInit(): void {
-    if(localStorage.getItem('user') !== null)
-    this.isSignedIn = true
-    else
-    this.isSignedIn = false
+    if (localStorage.getItem('user') !== null) this.isSignedIn = true;
+    else this.isSignedIn = false;
   }
 
   async onSignUp(email: string, password: string) {
-    await this.firebaseService.signup(email, password)
-    if(this.firebaseService.isLoggedIn)
-    this.isSignedIn = true
+    await this.firebaseService.signup(email, password);
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
   }
 
   async onSignIn(email: string, password: string) {
-    await this.firebaseService.signin(email, password)
-    if(this.firebaseService.isLoggedIn)
-    this.isSignedIn = true
+    await this.firebaseService.signin(email, password);
+    if (this.firebaseService.isLoggedIn) this.isSignedIn = true;
   }
 
-  handleLogout(){
-    this.isSignedIn = false
-  }
-  
-  logOut(){
-    this.firebaseService.logout
+  logOut() {
+    return this.afAuth.signOut().then(() => {
+      if (confirm('Are you sure you want to Log out ?')) {
+        this.isSignedIn = false;
+      }
+    });
   }
 }
